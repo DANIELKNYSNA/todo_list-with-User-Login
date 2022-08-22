4b61d2f754fdd3ce5e133031d0f1ec088ff2b13e
Daniel Murphy <knysnacoder@gmail.com>
2022-08-22 14:13:06 +0200

    Implementing the login page

--- base/views.py
+++ base/views.py
@@ -1,10 +1,20 @@
 from typing import Optional
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 
 from base.models import Task
 # Create your views here.
+
+class CustomLoginView(LoginView):
+  template_name: str = 'base/login.html'
+  fields = '__all__'
+  redirect_authenticated_user: bool = True
+
+  def get_success_url(self) -> str:
+    return reverse_lazy('tasks')
+
 class TaskList(ListView):
   model = Task
   context_object_name: Optional[str] = 'tasks'
@@ -30,3 +40,6 @@ class TaskDelete(DeleteView):
   model = Task
   context_object_name: Optional[str] = 'task'
   success_url: Optional[str] = reverse_lazy('tasks')
+
+
+  
