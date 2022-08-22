ee447bbe510b1573c382c81062cde8fa3c5f0233
Daniel Murphy <knysnacoder@gmail.com>
2022-08-22 12:33:44 +0200

    Implementing the create task

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 from base.models import Task
 # Create your views here.
@@ -13,3 +14,9 @@ class TaskDetail(DetailView):
   context_object_name: Optional[str] = 'task'
   template_name: str = 'base/task.html'
 
+class TaskCreate(CreateView):
+  model = Task
+  fields = '__all__'
+  success_url: Optional[str] = reverse_lazy('tasks')
+  # context_object_name: Optional[str] = 'create'
+  # template_name: str = 'base/create.html'
