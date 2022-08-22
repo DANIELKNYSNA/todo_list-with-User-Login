d2c82c094abbce2fa0e15f285db68c5d3d47ad54
Daniel Murphy <knysnacoder@gmail.com>
2022-08-22 11:25:18 +0200

    Implementing the details page

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from django.shortcuts import render
-from django.views.generic.list import ListView
+from django.views.generic import ListView, DetailView
 
 from base.models import Task
 # Create your views here.
@@ -8,4 +8,8 @@ class TaskList(ListView):
   model = Task
   context_object_name: Optional[str] = 'tasks'
 
-  
+class TaskDetail(DetailView):
+  model = Task
+  context_object_name: Optional[str] = 'task'
+  template_name: str = 'base/task.html'
+
