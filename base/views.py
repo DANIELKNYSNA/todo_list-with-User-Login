b6fc04cceef5480fc4993f39daf29007dbeacc46
Daniel Murphy <knysnacoder@gmail.com>
2022-08-22 13:03:36 +0200

    Implementing the delete functionality

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from base.models import Task
@@ -20,3 +20,13 @@ class TaskCreate(CreateView):
   success_url: Optional[str] = reverse_lazy('tasks')
   # context_object_name: Optional[str] = 'create'
   # template_name: str = 'base/create.html'
+
+class TaskUpdate(UpdateView):
+  model = Task
+  fields = '__all__'
+  success_url: Optional[str] = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+  model = Task
+  context_object_name: Optional[str] = 'task'
+  success_url: Optional[str] = reverse_lazy('tasks')
